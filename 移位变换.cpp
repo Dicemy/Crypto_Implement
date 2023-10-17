4d6c20dcdcc853790f42608508d3af91ec7447e0
Dicemy <1583925267@qq.com>
2023-10-17 21:24:47 +0800

    凯撒密码与移位变换密码

--- 移位变换.cpp
+++ 移位变换.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-string EX(string m) {
+string EX(string m, int k) {
 	string c;
 	c = m;
 	for(int i = 0; i < m.length(); i++) {
 		if (m[i] >= 'a' && m[i] <= 'z') {
-			c[i] = (m[i] - 'a' + 3) % 26 + 'a';
+			c[i] = (m[i] - 'a' + k) % 26 + 'a';
 		}else {
-			c[i] = (m[i] - 'A' + 3) % 26 + 'A';
+			c[i] = (m[i] - 'A' + k) % 26 + 'A';
 		}
 	}
 	return c;
 }
-string DX(string c) {
+string DX(string c, int k) {
 	string m;
 	m = c;
 	for(int i = 0; i < c.length(); i++) {
 		if (c[i] >= 'a' && c[i] <= 'z') {
-			m[i] = (c[i] - 'a' - 3) % 26 + 'a';
+			m[i] = (c[i] - 'a' - k) % 26 + 'a';
 		}else {
-			m[i] = (c[i] - 'A' - 3) % 26 + 'A';
+			m[i] = (c[i] - 'A' - k) % 26 + 'A';
 		}
 	}
 	return m;
@@ -27,17 +27,20 @@ string DX(string c) {
 int main(){
 	string in;
 	char flag;
+    int k = 0;
+    cout << "请输入加密密钥" << endl;
+    cin >> k;
 	while(1) {
-		cout << "请输入操作指令，1为加密，2为解密，其他为退出" << endl;        
+        cout << "请输入操作指令，1为加密，2为解密，其他为退出" << endl;        
         cin >> flag; //输入1进行加密，输入2进行解密，输入其他推出
 		if (flag == '1') {
             cout << "请输入需要加密的字符串：" << endl;
             cin >> in; //输入字符串
-			cout << EX(in) << endl; 
+			cout << EX(in, k) << endl; 
 		} else if (flag == '2') {
             cout << "请输入需要解密的字符串：" << endl;
             cin >> in; //输入字符串
-			cout << DX(in) << endl;
+			cout << DX(in, k) << endl;
 		} else break;
 	}
 	return 0;
